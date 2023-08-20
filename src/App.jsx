296f62509ff7da16720a00e03fee14789415a144
Ankit Acharjee <93594027+AnkitDcool-catcode@users.noreply.github.com>
2023-08-20 21:38:40 +0530

    console output fixed

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   const dispatch=useDispatch();
   const {url}=useSelector((state)=>
   state.home)
-  console.log(url)
+  // console.log(url)
   useEffect(()=>{
     fetchApiConfig();
     genresCall()
@@ -29,7 +29,7 @@ const App = () => {
     const fetchApiConfig = ()=>{
       fetchDataFromApi('/configuration')
        .then((res)=>{
-        console.log(res)
+        // console.log(res)
 
         const url={ backdrop:res.images.secure_base_url + "original",
           poster:res.images.secure_base_url + "original",
